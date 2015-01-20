b5ddc75a1844ca09c4208faa38db40585e356cac
Ben Parr <bparr@bparr.com>
2015-01-20 15:33:06 -0800

    Add script to aggregate a lists of exported ideas into a single list.

---
+++ journalapp/aggregate_ideas.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      description='Combine multiple idea exports into one.')
+  parser.add_argument('--file', type=file, required=True,
+                      help='File containing all idea exports (one per line).')
+  args = parser.parse_args()
+  lines = args.file.readlines()
+  args.file.close()
+
+  all_ideas = {}
+  for line in lines:
+    parsed = json.loads(line)
+    for idea in parsed:
+      if idea['now'] in all_ideas and idea['value'] != all_ideas[idea['now']]:
+        raise 'Mismatched idea values!'
+      all_ideas[idea['now']] = idea['value']
+
+  all_ideas_list = [{'now': x, 'value': y} for x,y in all_ideas.iteritems()]
+  print json.dumps(sorted(all_ideas_list, key=lambda x: x['now']),
+                   separators=(',', ':'))
+
+if __name__ == '__main__':
+  main()
