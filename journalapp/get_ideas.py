68286cfd7ebe19030645beb9668d089a70b5e65c
Ben Parr <bparr@bparr.com>
2016-12-29 02:46:08 -0500

    Hacks to get ideas.

--- journalapp/get_ideas.py
+++ journalapp/get_ideas.py
@@ -10,15 +10,43 @@ def main():
   parser.add_argument('--file', type=file, required=True,
                       help='File containing single JSON object.')
   parser.add_argument('--filter', type=str, required=False,
-                      help='Optional filter. Simple "in" matching if provided.')
+                      help='Optional filter. Space delimited. Simple "in" matching if provided.')
   args = parser.parse_args()
   lines = args.file.readlines()
   args.file.close()
 
+  previous = ''
+  previous2 = ''
+  previous3 = ''
+
+  idea_filters = [x.lower() for x in (args.filter or '').split(' ')]
+  all_ideas = dict()
+
   for x in json.loads(lines[0]):
     idea = x['value'].encode('utf8')
-    if args.filter is None or args.filter in idea:
-      print idea
+    # TODO this is all quite hacky. Clean up.
+    idea = idea.replace('##p3 ', '{{ ' + previous3 + ' }} ')
+    idea = idea.replace('##p2 ', '{{ ' + previous2 + ' }} ')
+    idea = idea.replace('##p ', '{{ ' + previous + ' }} ')
+    idea = idea.replace('##P ', '{{ ' + previous + ' }} ')
+    idea = idea.replace('#p ', '{{ ' + previous + ' }} ')
+    if idea.endswith('##p'):
+      idea += '{{ ' + previous + ' }}'
+
+
+    if 'selected' in x:
+      idea = idea.replace('##s ', '{{ ' + all_ideas[x['selected']] + ' }} ')
+
+    all_ideas[x['now']] = idea
+
+    for idea_filter in idea_filters:
+      if idea_filter in idea.lower():
+        print idea
+        break
+
+    previous3 = previous2
+    previous2 = previous
+    previous = idea
 
 if __name__ == '__main__':
   main()
